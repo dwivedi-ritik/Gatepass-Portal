6d8b6d29f06019ee5853ca9f0133530f04f04dec
dwivedi-ritik <dwi.ritik20@gmail.com>
2022-09-25 21:54:56 +0530

    Added Pie Chart , Added Mail Integration No validation for mail api No validation for mail restriction Removed Line chart

--- utils/constants.js
+++ utils/constants.js
@@ -5,69 +5,72 @@ export const gatePassStatus = {
 }
 
 export const NEXTAUTH_SECRET = "3aaAigN0x8IpUKMohN4G1f8mE7ZWedD6qenyYzY6x2Q="
-export const chartjsData = {
-    labels: [
-        "Jan",
-        "Feb",
-        "Mar",
-        "Apr",
-        "May",
-        "Jun",
-        "Jul",
-        "Aug",
-        "Sep",
-        "Oct",
-        "Nov",
-        "Dec"
-    ],
-    datasets: [
-        {
-            label: "Total Approval",
-            borderColor: '#818cf8',
-            backgroundColor: 'rgba(50, 115, 220, 0.1)',
-            fill: true,
-            data: [20, 30, 40, 15, 23, 45, 65, 78, 66, 44, 56, 67, 75],
-            lineTension: 0.4,
-            pointRadius: 2
-        },
-        {
-            label: "Total Rejected",
-            borderColor: '#F87171',
-            backgroundColor: 'rgba(255, 56, 96, 0.1)',
-            fill: true,
-            data: [20, 33, 41, 45, 13, 45, 65, 78, 66, 44, 56, 67, 75],
-            lineTension: 0.4,
-            pointRadius: 2
-        },
 
-    ]
-}
+//Configuration of line chart is commented out
 
-export const chartjsOption = {
-    maintainAspectRatio: false,
-    responsive: true,
-    scales: {
+// export const chartjsData = {
+//     labels: [
+//         "Jan",
+//         "Feb",
+//         "Mar",
+//         "Apr",
+//         "May",
+//         "Jun",
+//         "Jul",
+//         "Aug",
+//         "Sep",
+//         "Oct",
+//         "Nov",
+//         "Dec"
+//     ],
+//     datasets: [
+//         {
+//             label: "Total Approval",
+//             borderColor: '#818cf8',
+//             backgroundColor: 'rgba(50, 115, 220, 0.1)',
+//             fill: true,
+//             data: [20, 30, 40, 15, 23, 45, 65, 78, 66, 44, 56, 67, 75],
+//             lineTension: 0.4,
+//             pointRadius: 2
+//         },
+//         {
+//             label: "Total Rejected",
+//             borderColor: '#F87171',
+//             backgroundColor: 'rgba(255, 56, 96, 0.1)',
+//             fill: true,
+//             data: [20, 33, 41, 45, 13, 45, 65, 78, 66, 44, 56, 67, 75],
+//             lineTension: 0.4,
+//             pointRadius: 2
+//         },
 
-        y: {
-            ticks: {
-                font: {
-                    size: 8 //this change the font size
-                }
-            },
-            grid: {
-                display: false
-            },
+//     ]
+// }
 
-        },
-        x: {
-            ticks: {
-                font: {
-                    size: 8 //this change the font size
-                }
-            },
-            grid: {
-                display: false
-            }
-        }
-    }
-}
+// export const chartjsOption = {
+//     maintainAspectRatio: false,
+//     responsive: true,
+//     scales: {
+
+//         y: {
+//             ticks: {
+//                 font: {
+//                     size: 8 //this change the font size
+//                 }
+//             },
+//             grid: {
+//                 display: false
+//             },
+
+//         },
+//         x: {
+//             ticks: {
+//                 font: {
+//                     size: 8 //this change the font size
+//                 }
+//             },
+//             grid: {
+//                 display: false
+//             }
+//         }
+//     }
+// }
