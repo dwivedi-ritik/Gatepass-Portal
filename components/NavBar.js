2ad6ca7499628a05833c04a8f18037e42dae3bf5
dwivedi-ritik <dwi.ritik20@gmail.com>
2022-10-03 09:13:59 +0530

    Added maintenance search token Added dropdown Only Admin panel is left

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,7 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 
+const DropdownStatus = () => {
+    return (
+        <div class="absolute w-44 font-normal bg-white rounded top-10 -left-10">
+            <ul className="py-1 text-sm text-gray-700 dark:text-gray-400" >
+                <li>
+                    <Link href="/status">
+                        <a className="block py-2 pr-4 pl-3 text-gray-600 hover:bg-indigo-600 hover:text-white rounded">Gate Pass Status</a>
+                    </Link>
+                </li>
+                <li>
+                    <Link href='/maintenance/status/check'>
+                        <a className="block py-2 pr-4 pl-3 text-gray-600 hover:bg-indigo-600 hover:text-white rounded">Maintenance Status</a>
+                    </Link>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+const MobileDropdownStatus = () => {
+    return (
+        <div class="w-full font-normal rounded bg-gray-100">
+            <ul className="py-1 text-sm text-gray-700 dark:text-gray-400" >
+                <li>
+                    <Link href='/status'>
+                        <a className="block py-2 pr-4 pl-3 text-gray-600 hover:bg-indigo-600 hover:text-white rounded">Gate Pass Status</a>
+                    </Link>
+                </li>
+                <li>
+                    <Link href='maintenance/status/check'>
+                        <a className="block py-2 pr-4 pl-3 text-gray-600 hover:bg-indigo-600 hover:text-white rounded">Maintenance Status</a>
+                    </Link>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+
 export default function NavBar() { //TODOs Implement later
+    let [showDropdown, setShowDropdown] = useState(false)
     const toggleNav = () => {
         const toggler = document.getElementById("navbar-cta")
         if (toggler.classList.contains('hidden')) {
@@ -22,9 +62,13 @@ export default function NavBar() { //TODOs Implement later
                         <Link href="/maintenance">
                             <a className="hover:text-indigo-600 transition-colors">Upkeep</a>
                         </Link>
-                        <Link href="/status">
-                            <a className="hover:text-indigo-600 transition-colors">Status</a>
-                        </Link>
+                        <div className="relative cursor-pointer" onClick={() => setShowDropdown(!showDropdown)}>
+                            <a className="hover:text-indigo-600 transition-colors">Status
+                                <svg class="ml-1 w-5 h-5 inline" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                    <path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+                            </a>
+                            {showDropdown && <DropdownStatus />}
+                        </div>
                         <Link href="/admin/login" prefetch={false}>
                             <a className="hover:text-indigo-600 transition-colors">Admin</a>
                         </Link>
@@ -34,6 +78,7 @@ export default function NavBar() { //TODOs Implement later
                     </div>
                     <svg onClick={toggleNav} className="w-6 h-6 sm:hidden cursor-pointer" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
                 </div>
+
                 <div className="hidden flex justify-start items-center w-full sm:hidden" id="navbar-cta">
                     <ul className="flex flex-col py-4 mt-2 rounded-lg w-full font-semibold">
                         <li>
@@ -46,11 +91,14 @@ export default function NavBar() { //TODOs Implement later
                                 <a className="block py-2 pr-4 pl-3 text-gray-600 hover:bg-indigo-600 hover:text-white rounded">Upkeep</a>
                             </Link>
                         </li>
-                        <li>
-                            <Link href="/status">
-                                <a className="block py-2 pr-4 pl-3 text-gray-600 hover:bg-indigo-600 hover:text-white rounded">Status</a>
-                            </Link>
+                        <li onClick={() => setShowDropdown(!showDropdown)} className="cursor-pointer flex justify-between items-center text-gray-600  hover:bg-indigo-600 hover:text-white rounded">
+                            <p className="block py-2 pr-4 pl-3 ">Status
+                            </p>
+                            <svg class="mr-4 w-5 h-5 inline" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                <path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd"></path>
+                            </svg>
                         </li>
+                        {showDropdown && <MobileDropdownStatus />}
                         <li>
                             <Link href="/admin/login" prefetch={false}>
                                 <a className="block py-2 pr-4 pl-3 text-gray-600 hover:bg-indigo-600 hover:text-white rounded">Admin</a>
---
+++ pages/maintenance/status/check.js
@@ -0,0 +1,52 @@
+import React, { useRef } from "react";
+import Head from "next/head";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+import NavBar from "../../../components/NavBar"
+
+export default function CheckStatus() {
+    const tokenId = useRef("")
+    const router = useRouter()
+
+    const getTokenData = (e) => {
+        e.preventDefault()
+        router.push("/maintenance/" + tokenId.current.value)
+    }
+    return (
+        <div>
+            <Head>
+                <title>Check Maintenance status</title>
+                <meta lang="en"></meta>
+            </Head>
+            <NavBar />
+            <form className="w-full flex flex-wrap justify-center items-center mt-24 gap-2" onSubmit={getTokenData}>
+                <div className="md:w-1/2 w-3/4">
+                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" >
+                        Type Your Maintenance Token
+                    </label>
+                    <input ref={tokenId} className="appearance-none h-12 block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-indigo-600" id="grid-zip" type="text" placeholder=""></input>
+                </div>
+                <div onClick={getTokenData} >
+                    <a className="appearance-none cursor-pointer flex items-center gap-2 rounded border border-transparent h-12 mt-6 bg-indigo-600 px-4 py-2 font-medium text-white shadow-sm hover:bg-indigo-700  sm:text-sm">
+                        <p className="tracking-wide font-semibold" >Search</p>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+                        </svg>
+                    </a>
+                </div>
+            </form>
+            <div className="w-full flex justify-center mt-4 font-semibold cursor-pointer" href="/">
+                <div className='flex  items-center gap-1'>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5 text-indigo-700 rounded-lg">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+                    </svg>
+                    <Link href="/">
+                        <a className=' text-indigo-600 tracking-wide text-xs' >Back to home</a>
+                    </Link>
+                </div>
+            </div>
+
+        </div>
+    )
+}
