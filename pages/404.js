3c23049b3e81bfbd0f649fb806c9c38fb4baecde
dwivedi-ritik <dwi.ritik20@gmail.com>
2022-10-12 06:28:50 +0530

    added little 404 page production preview fix

--- pages/404.js
+++ pages/404.js
@@ -15,10 +15,10 @@ export default function Custom404() {
                     <h1
                         class="mt-6 text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl"
                     >
-                        Such Void!
+                        Such Void
                     </h1>
 
-                    <p class="mt-4 text-gray-400">We can't find that page.</p>
+                    <p class="mt-4 text-gray-400">We cannot find the page</p>
                 </div>
             </div>
 
