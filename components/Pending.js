bd2d1ca7317e7e995a13e3120b70d3b24d3a32a8
Ritik Dwivedi <dwi.ritik20@gmail.com>
2023-06-01 01:05:15 +0530

    lets hope for best2

--- components/Pending.js
+++ components/Pending.js
@@ -26,7 +26,7 @@ export default function Pending(props) {
                 </div>
                 <div className='mt-3 flex justify-center w-full'>
                     <Image
-                        text={HOSTED_URL + '/qr/verify/' + props.qrUrl}
+                        text={"https://gatepass.vercel.app" + '/qr/verify/' + props.qrUrl}
                         options={{
                             type: 'image/jpeg',
                             quality: 0.3,
